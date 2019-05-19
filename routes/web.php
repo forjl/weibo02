a3b821ecd73495140534c6489a7a87775ffc5ef3
forjl <748409412@qq.com>
2019-05-19 08:55:42 +0000

    用户页面显示

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,14 @@ Route::get('/help','StaticPagesController@help')->name('help');
 Route::get('/about','StaticPagesController@about')->name('about');
 
 Route::get('register','UsersController@create')->name('register');
+
+Route::get('/users','UsersController@index')->name('users.index');
+Route::get('/users/create','UsersController@create')->name('users.create');
+Route::post('/users','UsersController@store')->name('users.store');
+Route::get('users/{user}','UsersController@show')->name('users.show');
+Route::get('users/{user}/edit')->name('users.edit');
+Route::put('users/{user}','UsersController@update')->name('users.update');
+Route::delete('users/{user}','UsersController@destroy')->name('users.destroy');
+
+//Route::resource('users','UsersController');
+
